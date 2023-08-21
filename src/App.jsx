937d47c2ba79a99bddfc7ab6bb5f87bc8c129e22
ash730xo <ash730@gmail.com>
2023-08-21 13:20:41 -0400

    changing arrow function to regular function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-const App = () => {
+//arrow function 
+function App() {
+  // JSX 
   return (
     <div>This is our first component</div>
   )
 }
 
+// How to export component 
 export default App;
