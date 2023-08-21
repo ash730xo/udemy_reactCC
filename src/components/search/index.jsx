fbabc825d9fcda3ee1dce4f613430cb0e0f453ed
ash730xo <ash730@gmail.com>
2023-08-21 13:48:24 -0400

    useState and onChange for search and input

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import  { useState } from "react";
 import './syles.css'
 
 const Search = () => {
+    //state value and function 
+    const [inputValue, setInputValue] = useState('')
+
+    const handleInputValue = (event) => {
+        //gets the current value
+        const {value} = event.target;
+        //set the updated state
+        setInputValue(value)
+    }
+
+
+    console.log(inputValue)
+
     return (
         <div className="Search">
-            <input name="search" placeHolder="Search Recipes" id="Search"/>
+            <input name="search" onChange={handleInputValue} value={inputValue} placeholder="Search Recipes" id="Search"/>
             <button type="submit">Search</button>
         </div>
     )
