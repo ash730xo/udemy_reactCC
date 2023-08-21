1054bff026d93530dc46c8f2094d2fa7c62f4c1e
ash730xo <ash730@gmail.com>
2023-08-21 14:43:56 -0400

    API managing loading state

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "../../components/search";
 
 const dummydata = 'dummydata'
 
 const Homepage = () => {
+
+    //loading state
+    const [loadingState, setLoadingState]= useState(false)
+
+    //save results that we recieve from the API
+    const [recipes, setRecipes] = useState([])
+
     const getDataFromSearchComponent = (getData) => {
+        //keep the loading state as true before we are calling the API
+        setLoadingState(true)
+
         console.log(getData, 'getData')
 
         //calling API
         async function getReceipes(){
             const apiResponse = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=cc946bf2ecb84daaa5d492c140096313&query=${getData}`)
             const result = await apiResponse.json()
+            const {results} = result
+
+            if(results && results.length > 0) {
+                //set loading state as false again
+                //set the recipes state
+                setLoadingState(false)
+                setRecipes(results)
+            }
 
             console.log(result)
         }
 
         getReceipes()
     }
+
+    console.log(loadingState, recipes, `loadingState, recipes`)
 
     return (
         <div className="Homepage">
