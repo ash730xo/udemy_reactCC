01ff3f35f910b64a3b8c2a50db13628a93dac8d0
ash730xo <ash730@gmail.com>
2023-08-21 13:28:48 -0400

    updated app  & App classname style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import React from "react";
+import './App.css'
+
 //arrow function 
 function App() {
   // JSX 
   return (
-    <div>This is our first component</div>
+    <div className="App">This is our first component</div>
   )
 }
+
+
 
 // How to export component 
 export default App;
