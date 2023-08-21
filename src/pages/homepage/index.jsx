20a801d228e1985a303fc8a8dd2c1d09ffd9dd98
ash730xo <ash730@gmail.com>
2023-08-21 14:35:08 -0400

    async recipe API inplementation log results

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -6,6 +6,16 @@ const dummydata = 'dummydata'
 const Homepage = () => {
     const getDataFromSearchComponent = (getData) => {
         console.log(getData, 'getData')
+
+        //calling API
+        async function getReceipes(){
+            const apiResponse = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=cc946bf2ecb84daaa5d492c140096313&query=${getData}`)
+            const result = await apiResponse.json()
+
+            console.log(result)
+        }
+
+        getReceipes()
     }
 
     return (
